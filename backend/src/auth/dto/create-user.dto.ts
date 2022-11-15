7a8dd522ea8c33ea193de5431a601876df5bc192
Andres Ardila <andresardila33@gmail.com>
2022-11-15 09:45:02 -0500

    feat: add user DTO's

---
+++ backend/src/auth/dto/create-user.dto.ts
@@ -0,0 +1,47 @@
+import {
+  IsEmail,
+  IsString,
+  Matches,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+
+export class CreateUserDto {
+  @IsString()
+  @MinLength(1)
+  name: string;
+
+  @IsString()
+  @MinLength(1)
+  last_name: string;
+
+  @IsString()
+  @MinLength(1)
+  user_name: string;
+
+  @IsString()
+  @IsEmail()
+  @MinLength(1)
+  email: string;
+
+  @IsString()
+  @MinLength(6)
+  @MaxLength(50)
+  @Matches(/(?:(?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message:
+      'The password must have a Uppercase, lowercase letter and a number',
+  })
+  password: string;
+
+  @IsString()
+  @MinLength(1)
+  country: string;
+
+  @IsString()
+  @MinLength(1)
+  gmt: string;
+
+  @IsString()
+  @MinLength(1)
+  phone_number?: string;
+}
