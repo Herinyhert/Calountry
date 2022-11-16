fd7a1a2132b6ea54f61472a750229f5392bc846a
herinyhert <herinyhert@gmail.com>
2022-11-16 20:05:39 -0300

    Vue Router routes config
    se crean rutas para home y landing

---
+++ frontend/src/components/login/LoginForm.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <v-card>
+      <v-card-title> iniciar seccion </v-card-title>
+      <v-card-text>
+        <v-text-field label="label" v-model="data"></v-text-field>
+      </v-card-text>
+      <v-card-actions> </v-card-actions>
+    </v-card>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "LoginForm",
+  data() {
+    return {
+      data: " ",
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
