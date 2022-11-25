d7f59bbb1747f30ea38185458e2d55dd3f5e92c7
keven-correa <kturvi01@gmail.com>
2022-11-25 19:51:21 -0400

    many to many relationships btw users and groups

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
+import { GroupModule } from './group/group.module';
 
 @Module({
   imports: [
@@ -17,6 +18,7 @@ import { AuthModule } from './auth/auth.module';
       synchronize: true,
     }),
     AuthModule,
+    GroupModule,
   ],
 })
 export class AppModule {}
