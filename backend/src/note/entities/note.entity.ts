fdf8ab569da2f9ebde381db1b63f53be59b411e8
keven-correa <kturvi01@gmail.com>
2022-11-26 18:36:31 -0400

    Entity: Note

---
+++ backend/src/note/entities/note.entity.ts
@@ -0,0 +1,21 @@
+import { User } from "../../auth/entities/user.entity";
+import { Column, CreateDateColumn, Entity, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class Note {
+
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    @Column('varchar')
+    title: string;
+
+    @Column('varchar')
+    description: string;
+
+    @CreateDateColumn()
+    created_at: Date
+
+    @ManyToOne(() => User, (userNote) => userNote.notes)
+    user: User
+}
