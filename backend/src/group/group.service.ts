9b0ede99a6c938b1fecc8898978343cb3ee1dccb
keven-correa <kturvi01@gmail.com>
2022-11-26 01:48:22 -0400

    group service-refactor

--- backend/src/group/group.service.ts
+++ backend/src/group/group.service.ts
@@ -1,5 +1,9 @@
-
-import { BadRequestException, Inject, Injectable, InternalServerErrorException } from '@nestjs/common';
+import {
+  BadRequestException,
+  Inject,
+  Injectable,
+  InternalServerErrorException,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { AuthService } from 'src/auth/auth.service';
 import { Repository } from 'typeorm';
@@ -8,32 +12,24 @@ import { CreateGroupDto } from './dto/create-group.dto';
 import { UpdateGroupDto } from './dto/update-group.dto';
 import { Group } from './entities/group.entity';
 
-
 @Injectable()
 export class GroupService {
-
   constructor(
     @InjectRepository(Group)
     private readonly groupRepository: Repository<Group>,
-
-    // @InjectRepository(User)
-    // private readonly userRepository: Repository<User>,
-
     @Inject(AuthService)
     private readonly authService: AuthService,
-
-  ){}
+  ) {}
 
   async create(createGroupDto: CreateGroupDto) {
-    const {users = [], ...rest} = createGroupDto;
+    const { users = [], ...rest } = createGroupDto;
 
     const ids = await this.authService.findByIds(users);
     const saveGroup = await this.groupRepository.create({
       ...rest,
-      users: ids
+      users: ids,
     });
     return await this.groupRepository.save(saveGroup);
-    
   }
 
   async findAll() {
