17739bb440adb99614ef7507504de9044741277b
keven-correa <kturvi01@gmail.com>
2022-11-28 16:12:32 -0400

    main.ts cleanup

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -7,9 +7,9 @@ import { SecuritySchemeObject } from '@nestjs/swagger/dist/interfaces/open-api-s
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('/api');
-<<<<<<< HEAD
+
   app.enableCors();
-=======
+
   const config = new DocumentBuilder()
     .setDescription('Calountry backend')
     .setVersion('1.0')
@@ -24,7 +24,7 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
->>>>>>> 43339091d4b8c79a26c00f38fe0811e954c35748
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
