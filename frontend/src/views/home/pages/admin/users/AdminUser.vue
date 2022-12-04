473cf7e8ba9eb1dc1d5bb7c765da9a449fe5615e
Herinyhert <98710507+Herinyhert@users.noreply.github.com>
2022-12-04 19:41:18 -0300

    Update AdminUser.vue

--- frontend/src/views/home/pages/admin/users/AdminUser.vue
+++ frontend/src/views/home/pages/admin/users/AdminUser.vue
@@ -15,9 +15,7 @@
 </template>
 
 <script>
-// import { updateGroup } from "@/services/group/group";
 import { getUsers } from "@/services/user/user";
-// console.log("contengo todos los usuarios",getUsers());
 export default {
   name: "EditGroup",
   data() {
@@ -46,35 +44,6 @@ export default {
         console.log("quiero saber si estas aqui",this.allUsers);
         });
   },
-  // watch: {
-  //   "group.users"() {
-  //     this.toUpdate = this.group.users.map((x) => x.id);
-  //   },
-  // },
-  // methods: {
-  //   async handleSubmit() {
-  //     const group = { ...this.group };
-  //     group.users = this.toUpdate;
-  //     updateGroup(this.group.id, group).then((res) => {
-  //       console.log(res);
-  //       if (res.id == undefined) {
-  //         {
-  //           this.isError = true;
-  //           setTimeout(() => {
-  //             this.isError = false;
-  //           }, 1000);
-  //         }
-  //         return;
-  //       }
-
-  //       this.group = res;
-  //       this.updatedOk = true;
-  //       setTimeout(() => {
-  //         this.updatedOk = false;
-  //       }, 1000);
-  //     });
-  //   },
-  // },
 };
 </script>
 
